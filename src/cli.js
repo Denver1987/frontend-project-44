1f76386279e847a91ee1f2eb7bee9ee16de35900
Denver1987 <cm-den@yandex.ru>
2024-02-09 17:54:19 +0300

    Реализован запрос имени участника, небольшое исправление зависимости.

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+
+export function greetUser() {
+    const name = readlineSync.question('What is you name? ');
+    console.log('Hello, ' + name + '!');
+}
