fd63c9cae390af73ffb29f3d792e2503ef6fd439
Denver1987 <cm-den@yandex.ru>
2024-02-18 17:05:25 +0300

    Задокументирован модуль генерации чисел

--- src/service/numberGeneration.js
+++ src/service/numberGeneration.js
@@ -1,7 +1,17 @@
+/**
+ * Генерирует случайное число от 0 до 9007199254740990
+ * @returns {number}
+ */
 export function generateInteger() {
   return Math.round(Math.random() * Number.MAX_SAFE_INTEGER);
 }
 
+/**
+ * Генерирует случайное число из указанного диапазона
+ * @param {number} start - начало диапазона
+ * @param {number} end - конец диапазона
+ * @returns {number}
+ */
 export function generateFromRange(start, end) {
   return (generateInteger() % ((end - start) + 1)) + start;
 }
