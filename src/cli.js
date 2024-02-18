045ab72069b4c2be1b4159587096ee5741b9f17f
Denver1987 <cm-den@yandex.ru>
2024-02-18 19:53:16 +0300

    Доработка функции greetUser

--- src/cli.js
+++ src/cli.js
@@ -5,6 +5,7 @@ import readlineSync from 'readline-sync';
  * @returns {string}
  */
 export function greetUser() {
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
